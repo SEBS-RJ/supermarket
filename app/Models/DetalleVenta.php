f164968cb97c5a2db3b3bb135e5a84477a675eb3
SEBS-RJ <sebasramos123.2003@gmail.com>
2026-07-23 23:03:40 -0400

    fix: agregar trait HasFactory a los modelos

--- app/Models/DetalleVenta.php
+++ app/Models/DetalleVenta.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DetalleVenta extends Model
 {
+    use HasFactory;
     protected $table = 'detalles_venta';
 
     protected $fillable = [
