be75d5b403fdd5df2480cb63730871996830e144
SEBS-RJ <sebasramos123.2003@gmail.com>
2026-07-23 22:47:44 -0400

    merge: resolver conflicto en DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,13 +9,16 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
         $this->call([
-            CategoriaSeeder::class,
-            ProductoSeeder::class,
-            ClienteSeeder::class,
-            VentaSeeder::class,
+            CategoriaSeeder::class, // 1. sin dependencias
+            ProductoSeeder::class,  // 2. depende de categorias
+            ClienteSeeder::class,   // 3. sin dependencias
+            VentaSeeder::class,     // 4. depende de productos y clientes; genera ventas + detalles_venta
         ]);
     }
 }
